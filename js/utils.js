63d0d20a75f1cd9fb22e80b7427e1ee9efeabde3
IrynaShap <angeliawia@gmail.com>
2025-03-16 21:09:18 +0200

    Add mob-menu, adaptive

--- js/utils.js
+++ js/utils.js
@@ -28,21 +28,78 @@ document.addEventListener('DOMContentLoaded', function () {
     const carousels = document.querySelectorAll('.owl-carousel');
 
     carousels.forEach(carousel => {
-        const items = parseInt(carousel.getAttribute('data-items')) || 4;
-
+        const items = parseInt(carousel.getAttribute('data-items')) || 5;
         $(carousel).owlCarousel({
             autoplay: true,
             autoplayTimeout: 6500,
             autoplaySpeed: 1000,
             loop: true,
+            nav: true,
             responsive: {
                 0: {
                     items: 1
                 },
-                600: {
-                    items: items
+                768: {
+                    items: items-2
+                },
+                1200: {
+                    items: items-1
                 }
             }
         });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    let navBtn = this.querySelector(".nav-btn"),
+        navCloseBtn = this.querySelector(".nav-close-btn"),
+        toggleNav = newState => {
+            let attr = "aria-expanded",
+                state = navBtn.getAttribute(attr);
+
+            navBtn.setAttribute(attr,newState);
+        };
+    menuTab = e => {
+        let target = this.querySelector(".nav-btn[aria-expanded=true] ~ nav");
+
+        if (target !== null) {
+            let navLinks = target.querySelectorAll("a, button"),
+                tries = 0,
+                last = navLinks.length - 1;
+            // try to find focus in open nav
+            for (let l of navLinks) {
+                if (this.activeElement !== l)
+                    ++tries;
+            }
+            // put focus on X (first link) if outside or tabbing from last link
+            let onLast = this.activeElement === navLinks[last],
+                onFirst = this.activeElement === navLinks[0],
+                notShifting = !e.shiftKey,
+                shifting = e.shiftKey;
+
+            if (tries === navLinks.length || (onLast && notShifting)) {
+                e.preventDefault();
+                navLinks[0].focus();
+                // go to last link if shift-tabbing from X
+            } else if (onFirst && shifting) {
+                e.preventDefault();
+                navLinks[last].focus();
+            }
+        }
+    };
+
+    navBtn.addEventListener("click",() => {
+        toggleNav(true);
+    });
+    navCloseBtn.addEventListener("click",() => {
+        toggleNav(false);
+    });
+    this.addEventListener("keydown",e => {
+        // Esc
+        if (e.keyCode === 27)
+            toggleNav(false);
+        // Tab
+        else if (e.keyCode === 9)
+            menuTab(e);
     });
 });
